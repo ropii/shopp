b38c13d2147e59f71bd7bda8dbd92c99e5e7b308
roi <roro8823100@gmail.com>
2023-04-29 13:17:00 +0300

    commit

--- app/src/main/java/com/example/shop/functions/NotificationReceiver.java
+++ app/src/main/java/com/example/shop/functions/NotificationReceiver.java
@@ -18,36 +18,38 @@ import java.util.Random;
 
 public class NotificationReceiver extends BroadcastReceiver {
 
-
     @Override
     public void onReceive(Context context, Intent intent) {
         // Create a notification
 
-
+        // Get the system's notification manager
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-
+        // Check if there are any active notifications, and if so, skip the rest of the code
         if (notificationManager.getActiveNotifications().length > 0) {
             for (StatusBarNotification statusBarNotification : notificationManager.getActiveNotifications()) {
-                if (statusBarNotification.getId() == 1) {
+                if (statusBarNotification.getId() == 1) { // the notification with ID 1 has already been created
                     return;
                 }
             }
         }
 
+        // Create an intent to open the app's HomeActivity
         Intent homeIntent = new Intent(context, HomeActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        // Create a pending intent that will launch the HomeActivity when the notification is tapped
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, homeIntent, PendingIntent.FLAG_IMMUTABLE);
 
+        // Build the notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "notification_channel_id")
+                .setSmallIcon(R.drawable.logo) // Set the notification icon
+                .setContentTitle("Hi,") // Set the notification title
+                .setContentText("check the app, maybe new products have been uploaded") // Set the notification text
+                .setContentIntent(pendingIntent) // Set the intent that will be triggered when the notification is tapped
+                .setAutoCancel(true); // Set the notification to automatically dismiss when tapped
 
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "notification_channel_id")
-                    .setSmallIcon(R.drawable.logo)
-                    .setContentTitle("Hi,")
-                    .setContentText("check the app, maybe new products have been uploaded")
-                    .setContentIntent(pendingIntent)
-                    .setAutoCancel(true);
-            notificationManager.notify(1, builder.build());
-
-
+        // Show the notification
+        notificationManager.notify(1, builder.build());
     }
 }
--- app/src/main/java/com/example/shop/functions/NotificationService.java
+++ app/src/main/java/com/example/shop/functions/NotificationService.java
@@ -19,21 +19,27 @@ import java.util.Calendar;
 
 public class NotificationService extends Service {
 
+    // Constants for notification channel ID and name, and notification interval
     private static final String CHANNEL_ID = "notification_channel_id";
     private static final String CHANNEL_NAME = "notification_channel_name";
     private static final long NOTIFICATION_INTERVAL = 60*60 * 1000;
 
+    // The system's alarm manager to schedule notifications
     private AlarmManager alarmManager;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        // Get the system's alarm manager
         alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        // Create a notification channel for Android 8.0 and above
         NotificationChannel notificationChannel = null;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             notificationChannel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
         }
+        // Get the notification manager
         NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        // Register the notification channel with the notification manager
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             notificationManager.createNotificationChannel(notificationChannel);
         }
@@ -42,6 +48,7 @@ public class NotificationService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        // Schedule a notification when the service starts
         scheduleNotification();
         return super.onStartCommand(intent, flags, startId);
     }
@@ -52,10 +59,13 @@ public class NotificationService extends Service {
     }
 
     private void scheduleNotification() {
+        // Create an intent to broadcast when it's time to show the notification
         Intent notificationIntent = new Intent(this, NotificationReceiver.class);
+        // Create a pending intent to be triggered when the notification is shown
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT);
-
+        // Set the time when the notification should be shown
         long triggerTime = System.currentTimeMillis() + NOTIFICATION_INTERVAL;
+        // Schedule a repeating alarm to trigger the pending intent and show the notification
         alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, triggerTime, NOTIFICATION_INTERVAL, pendingIntent);
     }
 }
